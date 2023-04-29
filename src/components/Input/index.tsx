01e0ceeb92d26baddcefa1d54302a7c2f565363d
GabrielRuivo <gabriel-ruivo@hotmail.com>
2023-04-29 18:36:22 -0300

    feat: size input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -13,7 +13,7 @@ export const Input: FC<InputProps> = ({ itemType, placeholder, label }) => {
         {label}
       </label>
       <input
-        className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+        className="appearance-none block w-1/2 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
         id="input"
         type={itemType}
         placeholder={placeholder}
